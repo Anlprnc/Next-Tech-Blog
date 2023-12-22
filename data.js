1ea56f19b9ff63643fcc8ba3551a319f4c5822c1
Anlprnc <anilpirincciii@gmail.com>
2023-12-22 17:27:15 +0300

    oauth

---
+++ data.js
@@ -0,0 +1,42 @@
+export const postsData = [
+  {
+    id: '1',
+    title: 'New Smartphone Model Released',
+    content: 'A new smartphone model with advanced features has been released.',
+    author: 'John Doe',
+    datepublished: '2023-09-20',
+    category: 'Mobile',
+    links: ['https://www.example.com/news1'],
+    thumbnail:
+      'https://images.pexels.com/photos/1616516/pexels-photo-1616516.jpeg',
+  },
+  {
+    id: '2',
+    title: 'AI Breakthrough in Healthcare',
+    content:
+      'Artificial intelligence has discovered a new breakthrough in healthcare.',
+    author: 'Jane Smith',
+    datepublished: '2023-09-19',
+    category: 'Healthcare',
+    links: ['https://www.example.com/news2'],
+  },
+];
+
+export const categoriesData = [
+  {
+    id: '1',
+    name: 'Technology',
+  },
+  {
+    id: '2',
+    name: 'Tips & Tricks',
+  },
+  {
+    id: '3',
+    name: 'AI',
+  },
+  {
+    id: '4',
+    name: 'Programming',
+  },
+];

8991876086574f36fed68d0c9e226b1a8b267d02
Anlprnc <anilpirincciii@gmail.com>
2023-12-27 16:11:11 +0300

    tech-voyage-chronicle

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -31,16 +31,16 @@ export default function Navbar() {
   return (
     <div
       ref={popupRef}
-      className='flex justify-between pb-4 border-b mb-4 relative'
+      className="flex justify-between pb-4 border-b mb-4 relative"
     >
       <div>
         <Link href={'/'}>
-          <h1 className='text-dark text-4xl font-bold tracking-tighter'>
-            Tech News
+          <h1 className="text-dark text-4xl font-bold tracking-tighter">
+            Tech Voyage Chronicle
           </h1>
         </Link>
-        <p className='text-sm'>
-          Exploring Tomorrow's Innovations, <br /> One Byte at a Time
+        <p className="text-sm">
+          Unveiling Future Technologies, <br /> One Algorithm at a Time
         </p>
       </div>
 
@@ -51,45 +51,45 @@ export default function Navbar() {
               isPopupVisible ? 'flex' : 'hidden'
             }`}
           >
-            <div className='font-bold'>{session?.user?.name}</div>
+            <div className="font-bold">{session?.user?.name}</div>
             <div>{session?.user?.email}</div>
             <Link
               onClick={() => setIsPopupVisible(false)}
-              className='hover:underline'
+              className="hover:underline"
               href={'/dashboard'}
             >
               Dashboard
             </Link>
             <Link
               onClick={() => setIsPopupVisible(false)}
-              className='hover:underline'
+              className="hover:underline"
               href={'/create-post'}
             >
               Create Post
             </Link>
-            <button onClick={() => signOut()} className='btn'>
+            <button onClick={() => signOut()} className="btn">
               Sign Out
             </button>
           </div>
 
-          <div className='flex gap-2 items-center'>
+          <div className="flex gap-2 items-center">
             <Link
-              className='hidden md:flex gap-2 items-center mr-6'
+              className="hidden md:flex gap-2 items-center mr-6"
               href={'/create-post'}
             >
               <span>
                 <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  fill='none'
-                  viewBox='0 0 24 24'
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
                   strokeWidth={1.5}
-                  stroke='currentColor'
-                  className='w-6 h-6'
+                  stroke="currentColor"
+                  className="w-6 h-6"
                 >
                   <path
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                    d='M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z'
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
                   />
                 </svg>
               </span>
@@ -99,15 +99,15 @@ export default function Navbar() {
               src={session?.user?.image || ''}
               width={36}
               height={36}
-              alt='Profile Image'
-              className='rounded-full cursor-pointer'
+              alt="Profile Image"
+              className="rounded-full cursor-pointer"
               onClick={() => setIsPopupVisible((prev) => !prev)}
             />
           </div>
         </>
       ) : (
-        <div className='flex items-center'>
-          <Link className='btn' href={'/sign-in'}>
+        <div className="flex items-center">
+          <Link className="btn" href={'/sign-in'}>
             Sign In
           </Link>
         </div>

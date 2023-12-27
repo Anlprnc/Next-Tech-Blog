c03216af3747774fa118c45c187d0a4687d02217
Anlprnc <anilpirincciii@gmail.com>
2023-12-27 16:05:52 +0300

    finished

--- components/DeleteButton.tsx
+++ components/DeleteButton.tsx
@@ -1,6 +1,18 @@
 'use client';
 
+import toast from 'react-hot-toast';
+import { useRouter } from 'next/navigation';
+
 export default function DeleteButton({ id }: { id: string }) {
+  const router = useRouter();
+  const deleteImage = async (publicId: string) => {
+    const res = await fetch('/api/removeImage', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ publicId }),
+    });
+  };
+
   const handleDelete = async () => {
     const confirmed = window.confirm(
       'Are you sure you want to delete this post?'
@@ -16,16 +28,22 @@ export default function DeleteButton({ id }: { id: string }) {
         });
 
         if (res.ok) {
-          console.log('Post Deleted');
+          const post = await res.json();
+          const { publicId } = post;
+          await deleteImage(publicId);
+
+          toast.success('Post deleted successfully!');
+          router.refresh();
         }
       } catch (error) {
+        toast.error('Failed to delete post.');
         console.log(error);
       }
     }
   };
 
   return (
-    <button onClick={handleDelete} className='text-red-600'>
+    <button onClick={handleDelete} className="text-red-600">
       Delete
     </button>
   );
